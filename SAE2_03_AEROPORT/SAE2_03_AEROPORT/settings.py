3750290ee14fa8f8d3596ccb271284f0bed67712
Julian <julian.golde01@gmail.com>
2026-06-07 16:03:38 +0200

    update du projet

--- SAE2_03_AEROPORT/SAE2_03_AEROPORT/settings.py
+++ SAE2_03_AEROPORT/SAE2_03_AEROPORT/settings.py
@@ -79,11 +79,14 @@ WSGI_APPLICATION = 'SAE2_03_AEROPORT.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_prod',
+        'USER': 'root',
+        'PASSWORD': 'Sae203-Admin**',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
